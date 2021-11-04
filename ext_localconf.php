ee7420bfa8caa55405661d849f59690bc6f98218
Fabian Tief <fabian@priorist.com>
2021-11-04 14:19:11 +0100

    Migrate switchableControllerActions to separate plugins

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,33 +3,144 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-	'@import "EXT:edm/Configuration/TSConfig/Page/ContentElementWizard.tsconfig">'
-);
+call_user_func(
+	function () {
+		\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+			'Priorist.Edm',
+			'Pieventlist',
+			[
+				\Priorist\EdmTypo3\Controller\EventController::class => 'list',
+			],
+			// non-cacheable actions
+			[]
+		);
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'Priorist.Edm',
-	'Pi1',
-	[ // Comma-separated list of allowed actions per Controller. The first action of the first controller is the default action.
-		'Event'				=> 'list,detail,search',
-		'Location'		=> 'list,detail',
-		'Enrollment'	=> 'new',
-		'Participant'	=> 'login,logout,status',
-		'Newsletter'	=> 'add',
-		'Staff'				=> 'detail',
-		'Lecturer'		=> 'list,detail',
-		'FormError'		=> 'sendErrorMessage',
-	],
-	[ // specify which actions should not be cached
-		'Event'				=> 'detail',
-		'Location'		=> 'detail',
-		'Enrollment'	=> 'new',
-		'Participant'	=> 'login,logout,status',
-		'Newsletter'	=> 'add,subscribe',
-		'Staff'				=> 'detail',
-		'Lecturer'		=> 'detail',
-		'FormError'		=> 'sendErrorMessage',
-	]
+		\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+			'Priorist.Edm',
+			'Pieventdetail',
+			[
+				\Priorist\EdmTypo3\Controller\EventController::class => 'detail',
+			],
+			// non-cacheable actions
+			[
+				\Priorist\EdmTypo3\Controller\EventController::class => 'detail',
+			]
+		);
+
+		\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+			'Priorist.Edm',
+			'Pieventsearch',
+			[
+				\Priorist\EdmTypo3\Controller\EventController::class => 'search',
+			],
+			// non-cacheable actions
+			[]
+		);
+
+		\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+			'Priorist.Edm',
+			'Pilocationlist',
+			[
+				\Priorist\EdmTypo3\Controller\LocationController::class => 'list',
+			],
+			// non-cacheable actions
+			[]
+		);
+
+		\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+			'Priorist.Edm',
+			'Pilocationdetail',
+			[
+				\Priorist\EdmTypo3\Controller\LocationController::class => 'detail',
+			],
+			// non-cacheable actions
+			[
+				\Priorist\EdmTypo3\Controller\LocationController::class => 'detail',
+			]
+		);
+
+		\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+			'Priorist.Edm',
+			'Pienrollmentnew',
+			[
+				\Priorist\EdmTypo3\Controller\EnrollmentController::class => 'new',
+			],
+			// non-cacheable actions
+			[
+				\Priorist\EdmTypo3\Controller\EnrollmentController::class => 'new',
+			]
+		);
+
+		\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+			'Priorist.Edm',
+			'Piparticipantloginlogout',
+			[
+				\Priorist\EdmTypo3\Controller\ParticipantController::class => 'login, logout',
+			],
+			// non-cacheable actions
+			[
+				\Priorist\EdmTypo3\Controller\ParticipantController::class => 'login, logout',
+			]
+		);
+
+		\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+			'Priorist.Edm',
+			'Piparticipantstatus',
+			[
+				\Priorist\EdmTypo3\Controller\ParticipantController::class => 'status',
+			],
+			// non-cacheable actions
+			[
+				\Priorist\EdmTypo3\Controller\ParticipantController::class => 'status',
+			]
+		);
+
+		\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+			'Priorist.Edm',
+			'Pistaffdetail',
+			[
+				\Priorist\EdmTypo3\Controller\StaffController::class => 'detail',
+			],
+			// non-cacheable actions
+			[
+				\Priorist\EdmTypo3\Controller\StaffController::class => 'detail',
+			]
+		);
+
+		\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+			'Priorist.Edm',
+			'Pilecturerlist',
+			[
+				\Priorist\EdmTypo3\Controller\LecturerController::class => 'list',
+			],
+			// non-cacheable actions
+			[]
+		);
+
+		\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+			'Priorist.Edm',
+			'Pilecturerdetail',
+			[
+				\Priorist\EdmTypo3\Controller\LecturerController::class => 'detail',
+			],
+			// non-cacheable actions
+			[
+				\Priorist\EdmTypo3\Controller\LecturerController::class => 'detail',
+			]
+		);
+
+		\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+			'Priorist.Edm',
+			'Piformerrorsenderrormessage',
+			[
+				\Priorist\EdmTypo3\Controller\FormErrorController::class => 'sendErrorMessage',
+			],
+			// non-cacheable actions
+			[
+				\Priorist\EdmTypo3\Controller\FormErrorController::class => 'sendErrorMessage',
+			]
+		);
+	}
 );
 
 $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);

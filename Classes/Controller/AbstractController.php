966a1008ce45107e7f452fb970696ed41211e252
Fabian <fabian@priorist.com>
2021-11-19 12:37:14 +0100

    Refactor EDM client getter + setter (w/ registry storage)

--- Classes/Controller/AbstractController.php
+++ Classes/Controller/AbstractController.php
@@ -2,61 +2,89 @@
 
 namespace Priorist\EdmTypo3\Controller;
 
+use Exception;
 use Priorist\EDM\Client\Client;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Registry;
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
-class AbstractController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
+class AbstractController extends ActionController
 {
-	protected $client;
+	const REGISTRY_NAMESPACE = 'tx_edm';
+	const REGISTRY_KEY = 'cache_access-token';
+	const REGISTRY_EXPIRATION = 12; // expiration in hours
 
-	public function __construct(Client $client) {
-		$this->client = $client;
+	protected $client = null;
+	protected $registry;
+
+	public function __construct()
+	{
+		$this->registry = GeneralUtility::makeInstance(Registry::class);
 	}
 
-	private function setClient()
+	protected function storeAccessToken($accessToken)
 	{
-		$settings = $this->settings;
-		$url = $settings['edm']['url'];
-		$clientId = $settings['edm']['auth']['anonymous']['clientId'];
-		$clientSecret = $settings['edm']['auth']['anonymous']['clientSecret'];
-
-		$this->client = new Client($url, $clientId, $clientSecret);
-
-		return $this;
-	}
-
-	private function storeAccessToken($accessToken)
-	{
-		$registry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Registry::class);
 		$registryData = [
 			'access_token' => $accessToken,
 			'timestamp' => time(),
 		];
-		$registry->set('tx_edm', 'edm_cache_access-token', $registryData);
+		$this->registry->set(static::REGISTRY_NAMESPACE, static::REGISTRY_KEY, $registryData);
+	}
+
+	protected function getStoredAccessToken()
+	{
+		$edmCacheAccessToken = $this->registry->get(static::REGISTRY_NAMESPACE, static::REGISTRY_KEY);
+
+		if ($edmCacheAccessToken === false) {
+			return null;
+		}
+
+		$timestampOfPersistedAccessToken = $edmCacheAccessToken['timestamp'];
+		$currentTimestamp = time();
+		$expirationPeriodInSeconds = static::REGISTRY_EXPIRATION * 60 * 60;
+
+		if (($currentTimestamp - $expirationPeriodInSeconds) >= $timestampOfPersistedAccessToken) {
+			$this->registry->remove(static::REGISTRY_NAMESPACE, static::REGISTRY_KEY);
+			return null;
+		}
+
+		return $edmCacheAccessToken['access_token'];
 	}
 
 	public function getClient()
 	{
-		$registry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Registry::class);
+		if ($this->client === null) {
+			$settings = $this->settings;
 
-		if (false === $edmCacheAccessToken = $registry->get('tx_edm', 'edm_cache_access-token')) {
-			// no access token has been set yet
-			$this->setClient();
-			$this->storeAccessToken($this->client->getAccessToken());
-		} else {
-			$timestampOfPersistedAccessToken = $edmCacheAccessToken['timestamp'];
-			$currentTimestamp = time();
-			$expirationPeriod = 43200; // 12 hours in seconds
-
-			if (($currentTimestamp - $expirationPeriod) >= $timestampOfPersistedAccessToken) {
-				// set access token has expired
-				$registry->remove('tx_edm', 'edm_cache_access-token');
-				$this->setClient();
-				$this->storeAccessToken($this->client->getAccessToken());
-			} else {
-				// set access token is still valid
-				$persistedAccessToken = $edmCacheAccessToken['access_token'];
-				$this->client->setAccessToken($persistedAccessToken); // $this->client currently null
+			if ($settings === null) {
+				throw new Exception('EDM Extension TypoScript is not configured properly.');
 			}
+
+			if ($settings['edm']['url'] === '{$plugin.tx_edm.edm.url}') {
+				throw new Exception('No EDM URL has been defined in TypoScript constants.');
+			}
+
+			if ($settings['edm']['auth']['anonymous']['clientId'] === '{$plugin.tx_edm.edm.auth.anonymous.clientId}') {
+				throw new Exception('No EDM Client ID has been defined in TypoScript constants.');
+			}
+
+			if ($settings['edm']['auth']['anonymous']['clientSecret'] === '{$plugin.tx_edm.edm.auth.anonymous.clientSecret}') {
+				throw new Exception('No EDM Client Secret has been defined in TypoScript constants.');
+			}
+
+			$url = $settings['edm']['url'];
+			$clientId = $settings['edm']['auth']['anonymous']['clientId'];
+			$clientSecret = $settings['edm']['auth']['anonymous']['clientSecret'];
+
+			$this->client = new Client($url, $clientId, $clientSecret);
+		}
+
+		$storedAccessToken = $this->getStoredAccessToken();
+
+		if ($storedAccessToken !== null) {
+			$this->client->setAccessToken($storedAccessToken);
+		} else {
+			$this->storeAccessToken($this->client->getAccessToken());
 		}
 
 		return $this->client;
